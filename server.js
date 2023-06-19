5c3dd37b8ab49c4ebfe4b1c43d64b9630fce8952
ButterBoyJulian <34288129+ButterDebugger@users.noreply.github.com>
2023-06-19 18:31:30 -0400

    Added compression

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
+import compression from "compression";
 import path from "node:path";
 import http from "node:http";
 import dotenv from "dotenv";
@@ -14,6 +15,7 @@ const app = express();
 const server = http.Server(app);
 
 // Add middleware
+app.use(compression());
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use((req, res, next) => {
