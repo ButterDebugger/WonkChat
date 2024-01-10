2000aa5eb9f83b90f0967af2d44168efa0aefbdc
ButterDebugger <34288129+ButterDebugger@users.noreply.github.com>
2024-01-10 04:05:35 +0000

    Deleted folder

--- client/README.md
+++
@@ -1,3 +0,0 @@
-# WonkClient
-
-This repository contains code for [WonkChat](https://github.com/ButterDebugger/WonkChat)'s frontend.
--- client/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-
-app.use(express.static(path.join(process.cwd(), "public"), {
-  extensions: ['html', 'htm']
-}));
-
-app.listen(4030, () => console.log('Frontend server is running'));
