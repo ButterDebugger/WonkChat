3448455991dad3c75988a795b898bd2f2a2ec228
ButterDebugger <34288129+ButterDebugger@users.noreply.github.com>
2024-06-25 16:30:37 -0400

    Removed dotenv dependency

--- src/server.js
+++ src/server.js
@@ -1,30 +1,43 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
-import { router as attachmentsRoute, clean as cleanAttachments } from "./attachments.js";
+import {
+	router as attachmentsRoute,
+	clean as cleanAttachments
+} from "./attachments.js";
 import { getStreamRoute } from "./streams.js";
 import chalk from "chalk";
 import http from "node:http";
 import cors from "cors";
-import dotenv from "dotenv";
 import { authRouter, authenticate } from "./auth.js";
 import { router as gatewayRoute } from "./gateway.js";
 import { createRoom } from "./data.js";
 
-dotenv.config();
 const port = process.env.PORT ?? 5001;
 const app = express();
 const server = http.createServer({}, app).listen(port);
 
+console.log(process.env.TOKEN_SECRET);
+
 // Add server log listeners
 server.addListener("listening", () => {
-    console.log(chalk.bgGreen.bold(" LISTENING "), chalk.white(`API server is running on port ${port}`));
+	console.log(
+		chalk.bgGreen.bold(" LISTENING "),
+		chalk.white(`API server is running on port ${port}`)
+	);
 });
 server.addListener("close", () => {
-    console.log(chalk.bgYellow.bold(" CLOSE "), chalk.white("API server has closed"));
+	console.log(
+		chalk.bgYellow.bold(" CLOSE "),
+		chalk.white("API server has closed")
+	);
 });
 server.addListener("error", (err) => {
-    console.error(chalk.bgRed.bold(" ERROR "), chalk.white("API server has encountered an error:"), err);
+	console.error(
+		chalk.bgRed.bold(" ERROR "),
+		chalk.white("API server has encountered an error:"),
+		err
+	);
 });
 
 // Add middleware
@@ -34,10 +47,10 @@ app.use(cors());
 
 // Ping route
 app.get("/ping", (req, res) => {
-    res.status(200).json({
-        message: "Pong!",
-        success: true
-    });
+	res.status(200).json({
+		message: "Pong!",
+		success: true
+	});
 });
 
 // Auth routes
@@ -58,9 +71,9 @@ createRoom("wonk", "Welcome to Wonk Chat!");
 
 // Unknown endpoint handler
 app.use((req, res, next) => {
-    res.status(400).json({
-        error: true,
-        message: "Unknown endpoint",
-        code: 105
-    })
+	res.status(400).json({
+		error: true,
+		message: "Unknown endpoint",
+		code: 105
+	});
 });
