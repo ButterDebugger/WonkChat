21ffee702ede64ced96bc63a07dae5f290960bc3
ButterDebugger <34288129+ButterDebugger@users.noreply.github.com>
2025-05-02 01:09:38 -0400

    Added database migrations

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -1,4 +1,11 @@
-import { Kysely, type Generated } from "kysely";
+import fs from "node:fs/promises";
+import path from "node:path";
+import {
+	FileMigrationProvider,
+	Kysely,
+	Migrator,
+	type Generated
+} from "kysely";
 import { LibsqlDialect } from "@libsql/kysely-libsql";
 import {
 	database_auth_Token,
@@ -15,6 +22,41 @@ export const db = new Kysely<Database>({
 		syncInterval: 60 // Sync every minute
 	})
 });
+
+// Migrate database to the latest version
+const migrator = new Migrator({
+	db,
+	provider: new FileMigrationProvider({
+		fs,
+		path,
+		migrationFolder: path.join(__dirname, "./migrations")
+	})
+});
+
+const { error, results } = await migrator.migrateToLatest();
+
+if (error || typeof results === "undefined") {
+	console.error("Failed to migrate database", error);
+	process.exit(1);
+}
+
+for (const it of results) {
+	switch (it.status) {
+		case "Success":
+			console.log(
+				`Migration "${it.migrationName}" was executed successfully`
+			);
+			break;
+		case "NotExecuted":
+			console.log(
+				`"${it.migrationName}" was skipped because it was already executed`
+			);
+			break;
+		case "Error":
+			console.error(`Failed to execute migration "${it.migrationName}"`);
+			break;
+	}
+}
 
 // Declare database types
 export interface Database {
---
+++ src/lib/migrations/2025-05-01_init.ts
@@ -0,0 +1,28 @@
+import type { Kysely } from "kysely";
+
+export async function up(db: Kysely<unknown>): Promise<void> {
+	await db.schema
+		.createTable("users")
+		.addColumn("username", "text", (col) => col.primaryKey())
+		.addColumn("displayName", "text", (col) => col.notNull())
+		.addColumn("password", "text", (col) => col.notNull())
+		.addColumn("color", "text", (col) => col.notNull())
+		.addColumn("rooms", "jsonb", (col) => col.notNull())
+		.addColumn("online", "boolean", (col) => col.notNull())
+		.addColumn("publicKey", "blob")
+		.execute();
+
+	await db.schema
+		.createTable("rooms")
+		.addColumn("name", "text", (col) => col.primaryKey())
+		.addColumn("description", "text", (col) => col.notNull())
+		.addColumn("members", "jsonb", (col) => col.notNull())
+		.addColumn("publicKey", "blob", (col) => col.notNull())
+		.addColumn("privateKey", "blob", (col) => col.notNull())
+		.execute();
+}
+
+export async function down(db: Kysely<unknown>): Promise<void> {
+	await db.schema.dropTable("users").execute();
+	await db.schema.dropTable("rooms").execute();
+}
