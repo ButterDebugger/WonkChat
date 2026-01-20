02341ca4958e59a5e3f5f32bef765cf56834a7e7
ButterDebugger <34288129+ButterDebugger@users.noreply.github.com>
2026-01-20 00:27:51 -0500

    Increased max chunk size

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -30,4 +30,4 @@ if (!Bun.env.S3_ACCESS_KEY_ID) throw new Error("S3_ACCESS_KEY_ID is not set");
 
 // TODO: make these constants configurable
 export const maxUploadAge = 1000 * 60 * 60 * 6; // 6 hours
-export const maxChunkSize = 1024 * 1024; // 1 MB
+export const maxChunkSize = 1024 * 1024 * 50; // 50 MB
